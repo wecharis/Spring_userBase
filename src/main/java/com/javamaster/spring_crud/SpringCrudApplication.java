cce1f4b7fedadb701ade2a3d7430e96c45a358e3
wecharis <wecharis@student.21-school.ru>
2020-10-22 14:58:03 +0300

    v.0.1

--- src/main/java/com/javamaster/spring_crud/SpringCrudApplication.java
+++ src/main/java/com/javamaster/spring_crud/SpringCrudApplication.java
@@ -8,6 +8,7 @@ public class SpringCrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCrudApplication.class, args);
+//        https://java-master.com/spring-boot-%D0%B2%D0%B5%D0%B1-%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-%D1%81-%D0%BD%D1%83%D0%BB%D1%8F/
     }
 
 }
